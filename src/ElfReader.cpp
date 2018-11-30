1052854a11efd5d61bd6fea2325669b0b1346520
rhirufxmbcyj <liu_wan001@163.com>
2018-11-30 18:04:19 +0800

    添加界面分布

--- src/ElfReader.cpp
+++ src/ElfReader.cpp
@@ -4,4 +4,55 @@ ElfReader::ElfReader(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	//设置splitter比例 左边占2份 右边占5份
+	ui.splitter_left->setStretchFactor(0, 2);
+	ui.splitter_left->setStretchFactor(1, 5);
+	//连接analyze信号槽
+	QObject::connect(this, SIGNAL(start_analyze(QString)), this, SLOT(start_analyze_slot(QString)));
+}
+
+void ElfReader::clean_up_data()
+{
+	if (file)
+	{
+		delete file;
+		file = NULL;
+	}
+	//清空list tree控件
+}
+
+void ElfReader::dropEvent(QDropEvent *event)
+{
+	QString file_name = event->mimeData()->urls().first().toLocalFile();
+	//qDebug() << file_name;
+	emit start_analyze(file_name);
+}
+
+void ElfReader::dragEnterEvent(QDragEnterEvent *event)
+{
+	event->acceptProposedAction();
+}
+
+void ElfReader::pushButton_open_clicked()
+{
+	QString file_name = QFileDialog::getOpenFileName(this,u8"请选择一个ELF文件","","",0);
+	//qDebug() << file_name;
+	emit start_analyze(file_name);
+}
+
+void ElfReader::pushButton_save_clicked()
+{
+	qDebug() << "save";
+}
+
+void ElfReader::start_analyze_slot(QString file_name)
+{
+	//qDebug() << file_name;
+	clean_up_data();
+	QFile f(file_name);
+	if (!f.open(QIODevice::ReadOnly))
+	{
+		QMessageBox::warning(NULL, MESSAGE_OPEN_FILE_ERROR, MESSAGE_CAPTION);
+	}
+	//读文件 最好读到char*里边
 }
--- src/ElfReader.h
+++ src/ElfReader.h
@@ -1,15 +1,35 @@
-#pragma once
+#ifndef ELF_READER_H
+#define ELF_READER_H
 
 #include <QtWidgets/QMainWindow>
 #include "ui_ElfReader.h"
-
+#include "define.h"
+//拖拽取文件路径
+#include <QDragEnterEvent>
+#include <QDropEvent>
+#include <QMimeData>
+//文件选择对话框
+#include <QFileDialog>
+#include <QMessageBox>
 class ElfReader : public QMainWindow
 {
 	Q_OBJECT
 
 public:
 	ElfReader(QWidget *parent = Q_NULLPTR);
-
+	void clean_up_data();
 private:
-	Ui::ElfReaderClass ui;
+	Ui::ElfReader ui;
+	char *file;
+protected:
+	void dropEvent(QDropEvent *event);
+	void dragEnterEvent(QDragEnterEvent *event);
+public slots:
+	void pushButton_open_clicked();
+	void pushButton_save_clicked();
+	void start_analyze_slot(QString file_name);
+signals:
+	void start_analyze(QString file_name);
 };
+
+#endif
